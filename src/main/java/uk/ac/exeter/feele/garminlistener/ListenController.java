3d52028e0cd26efc3b094d81f457dc9a81817f65
Scott Vincent <s.j.vincent@exeter.ac.uk>
2025-11-10 11:28:28 +0000

    Get data from POST request body

--- src/main/java/uk/ac/exeter/feele/garminlistener/ListenController.java
+++ src/main/java/uk/ac/exeter/feele/garminlistener/ListenController.java
@@ -3,6 +3,7 @@ package uk.ac.exeter.feele.garminlistener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -17,8 +18,8 @@ public class ListenController {
     }
 
     @PostMapping("/listen")
-    public String listenPost(@RequestParam Map<String, String> allParams) {
-        System.out.println("Received POST params: " + allParams);
-        return "Received POST params: " + allParams;
+    public String listenPost(@RequestBody String jsonBody) {
+        System.out.println("Received POST JSON: " + jsonBody);
+        return "Received POST JSON: " + jsonBody;
     }
 }
