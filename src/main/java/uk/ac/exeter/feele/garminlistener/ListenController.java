f2ba3edebddff3f30cfa45592119d29500b9c6df
Scott Vincent <s.j.vincent@exeter.ac.uk>
2025-11-14 16:07:06 +0000

    Added deregister and permission change end points

--- src/main/java/uk/ac/exeter/feele/garminlistener/ListenController.java
+++ src/main/java/uk/ac/exeter/feele/garminlistener/ListenController.java
@@ -1,6 +1,7 @@
 package uk.ac.exeter.feele.garminlistener;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,6 +27,38 @@ public class ListenController {
         }
         int duration = (int)(System.currentTimeMillis() - startTime);
         System.out.println("Handled request in: " + duration + "ms - ");
-        return "Received POST JSON: " + jsonBody;
+        return "OK";
+    }
+
+    @DeleteMapping("/deregister")
+    public String deregister(@RequestParam Map<String, String> params) {
+        int startTime = (int)(System.currentTimeMillis());
+        try {
+            System.out.println("Received DELETE params: " + params);
+            // Not sure we need to do anything here? Garmin is just letting us know, and it will
+            // stop sending data. We had the permission of the user to collect the data we have, and
+            // I think we can keep it unless they specifically ask us to delete it.
+        } catch (Exception e) {
+            System.out.println("Failed to handle DELETE: " + e.getMessage());
+        }
+        int duration = (int)(System.currentTimeMillis() - startTime);
+        System.out.println("Handled request in: " + duration + "ms - ");
+        return "OK";
+    }
+
+    @GetMapping("/permissionChange")
+    public String permissionChange(@RequestParam Map<String, String> params) {
+        int startTime = (int)(System.currentTimeMillis());
+        try {
+            System.out.println("Received permissionChange, params: " + params);
+            // Not sure we need to do anything here? Garmin is just letting us know, and it will
+            // stop sending data. We had the permission of the user to collect the data we have, and
+            // I think we can keep it unless they specifically ask us to delete it.
+        } catch (Exception e) {
+            System.out.println("Failed to handle permissionChange: " + e.getMessage());
+        }
+        int duration = (int)(System.currentTimeMillis() - startTime);
+        System.out.println("Handled request in: " + duration + "ms - ");
+        return "OK";
     }
 }
